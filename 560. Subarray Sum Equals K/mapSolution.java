2336c83699156b2e003777756e06031cb944c62b
Ryo Oshima <ryo.oshima@ibm.com>
2024-03-27 23:27:01 +0900

    指摘修正

--- 560. Subarray Sum Equals K/mapSolution.java
+++ 560. Subarray Sum Equals K/mapSolution.java
@@ -2,18 +2,17 @@ class Solution {
     // 修正回答、ちゃんとアルゴリズムを考えて行う必要がある。おそらく面接ではO(n^2)だともう少し改善できますかと言われると思うので5分くらいでO(n^2)でといて、そこから改善した答えを10分とかでできると良さそう
     //
     public int subarraySum(int[] nums, int k) {
-        int sumEqualToKWithSection = 0;
-        Map<Integer, Integer> sumForSection = new HashMap<>();
-        sumForSection.put(0, 1);
-        int countForSumEqualK = 0;
+        int targetSubArrayCount = 0;
+        Map<Integer, Integer> prefixSumToCount = new HashMap<>();
+        prefixSumToCount.put(0, 1);
+        int prefixSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            countForSumEqualK += nums[i];
-            if (sumForSection.containsKey(countForSumEqualK - k)) {
-                sumEqualToKWithSection += sumForSection.get(countForSumEqualK - k);
+            prefixSum += nums[i];
+            if (prefixSumToCount.containsKey(prefixSum - k)) {
+                targetSubArrayCount += prefixSumToCount.getOrDefault(prefixSum - k, 0);
             }
-            sumForSection.put(countForSumEqualK,
-                    sumForSection.getOrDefault(countForSumEqualK, 0) + 1);
+            prefixSumToCount.put(prefixSum, prefixSumToCount.getOrDefault(prefixSum, 0) + 1);
         }
-        return sumEqualToKWithSection;
+        return targetSubArrayCount;
     }
 }
