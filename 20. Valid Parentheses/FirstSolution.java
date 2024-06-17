65e8cc76344f0f18bdefb997607e04bf7d802715
Ryo Oshima <ryo.oshima@ibm.com>
2024-06-18 01:09:01 +0900

    指摘修正

--- 20. Valid Parentheses/FirstSolution.java
+++ 20. Valid Parentheses/FirstSolution.java
@@ -1,24 +1,24 @@
 public class FirstSolution {
     public boolean isValid(String s) {
-        Stack<Character> opanCharacters = new Stack<>();
+        Stack<Character> openCharacters = new Stack<>();
         char[] cs = s.toCharArray();
         for (char current : cs) {
-            if (opanCharacters.isEmpty() || isOpenCharacter(current)) {
-                opanCharacters.push(current);
-            } else if (isPare(opanCharacters.peek(), current)) {
-                opanCharacters.pop();
+            if (openCharacters.isEmpty() || isOpenCharacter(current)) {
+                openCharacters.push(current);
+            } else if (isPair(openCharacters.peek(), current)) {
+                openCharacters.pop();
             } else {
                 return false;
             }
         }
-        return opanCharacters.isEmpty();
+        return openCharacters.isEmpty();
     }
 
     private boolean isOpenCharacter(char c) {
-        return (c == '(' || c == '{' || c == '[');
+        return c == '(' || c == '{' || c == '[';
     }
 
-    private boolean isPare(char left, char right) {
+    private boolean isPair(char left, char right) {
         boolean result;
         switch (left) {
             case '(':
