e56530d4d25edb67ee6fe2881f660951d247545a
ryo <34418898+ryoooooory@users.noreply.github.com>
2024-08-25 15:51:23 +0900

    Update step1-1.java

--- 349. Intersection of Two Arrays/step1-1.java
+++ 349. Intersection of Two Arrays/step1-1.java
@@ -1,19 +1,19 @@
 class Solution {
     // [1,2,3] [2,3,4] -> [2,3]
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> nums = new HashSet<>();
+        Set<Integer> nums1Elements = new HashSet<>();
         for(int num : nums1) {
-            nums.add(num);
+            nums1Elements.add(num);
         }
-        Set<Integer> duplicates = new HashSet<>();
+        Set<Integer> elementsInNums1AndNums2 = new HashSet<>();
         for(int num : nums2) {
-            if (nums.contains(num)) {
-                duplicates.add(num);
+            if (nums1Elements.contains(num)) {
+                elementsInNums1AndNums2.add(num);
             }
         }
-        int[] intersection = new int[duplicates.size()];
+        int[] intersection = new int[elementsInNums1AndNums2.size()];
         int index = 0;
-        for(int num : duplicates) {
+        for(int num : elementsInNums1AndNums2) {
             intersection[index++] = num;
         }
         return intersection;
