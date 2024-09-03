265ef5b40fabc7d257d142e0f7582eeb2483cc72
ryo <34418898+ryoooooory@users.noreply.github.com>
2024-09-03 23:36:39 +0900

    Update step2-1.java

--- 1. Two Sum/step2-1.java
+++ 1. Two Sum/step2-1.java
@@ -2,7 +2,7 @@ class Solution {
     // [1, 2, 3] t = 4  => ans = [0,2]
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> numToIndex = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int currentNum = nums[i];
             int complement = target - currentNum;
             if (numToIndex.containsKey(complement)) {
