df475cffc16b373a28f534de36b5998ae9920ffe
ryo <34418898+ryoooooory@users.noreply.github.com>
2024-09-12 23:44:14 +0900

    Update step1-1.java

--- 387. First Unique Character in a String/step1-1.java
+++ 387. First Unique Character in a String/step1-1.java
@@ -2,11 +2,11 @@ class Solution {
     // leetcode 
     public int firstUniqChar(String s) {
         Map<Character, Integer> indices = new HashMap<>();
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             indices.put(c, indices.getOrDefault(c, 0) + 1);
         }
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (indices.get(c) == 1) {
                 return i;
